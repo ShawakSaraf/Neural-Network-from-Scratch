5dc8895e3b024c3f80bc5466f0290bbf2d349ae6
Shawak9 <shawaksaraf456@gmail.com>
2022-12-09 13:47:04 +0530

    Refactoring code and fixing some bugs

--- Multi_Layer_Perceptron.py
+++ Multi_Layer_Perceptron.py
@@ -1,13 +1,13 @@
 # type: ignore
+from matplotlib.style import available
 import numpy as np
 import random, time, json, sys, os
-from pprint import pprint
 from tqdm import tqdm
 
 curr_dir = os.path.dirname( os.path.abspath( __file__ ) )
 trained_model_file = curr_dir+r'\TrainedModelData.sav'
 
-class CrossEntropyCost(object):
+class CrossEntropyCost( object ):
 	@staticmethod
 	def func(a, y):
 		return np.sum( np.nan_to_num( -y*np.log(a) - (1-y) * np.log(1-a) ) )
@@ -16,25 +16,109 @@ class CrossEntropyCost(object):
 	def delta(a, y):
 		return ( a - y )
 
-class MLP(object):
-	def __init__( self, layers=[784, 30, 10], cost=CrossEntropyCost ):
-		self.num_Layers = len(layers)
-		self.layers     = layers
-		self.cost       = cost
-		self.weights    = [ 
+class Sigmoid( object ):
+	def __call__( self, z ):
+		return 1.0 / ( 1.0 + np.exp(-z) )
+
+	def prime( self, z ):
+		return self( z ) * ( 1.0 - self( z ) )
+
+# Stochastic gradient decent
+class SGD():
+	def __init__(self, model=None ):
+		self.learning_rate = model.learning_rate
+		self.lmbda         = model.lmbda
+		self.cost          = model.cost
+		self.activation    = model.activation
+		self.num_layers    = model.num_layers
+		self.model         = model
+
+	def __call__( self, batch, num_data ):
+		nabla_W = [ np.zeros( w.shape ) for w in self.model.weights ]
+		nabla_B = [ np.zeros( b.shape ) for b in self.model.biases ]
+
+		for inputs, labels in batch:
+			delta_Nabla_W, delta_Nabla_B = self.propagate(inputs, labels)
+
+			# Summing up all the changes in weights amd biases
+			nabla_W = [ nw + d_nw for nw, d_nw in zip( nabla_W, delta_Nabla_W ) ]
+			nabla_B = [ nb + d_nb for nb, d_nb in zip( nabla_B, delta_Nabla_B ) ]
+
+		# Taking steps towards the lowest cost function
+		# lmbda, regularization parameter
+		w_coef  = 1 - self.learning_rate * self.lmbda / num_data
+		nm_coef = self.learning_rate / len(batch)
+
+		self.model.weights = [
+			( w_coef * w ) - ( nm_coef * nw )
+			for w, nw in zip( self.model.weights, nabla_W )
+		]
+
+		self.model.biases = [ 
+			b - ( self.learning_rate / len(batch) ) * nb
+			for b, nb in zip( self.model.biases, nabla_B ) 
+		]
+
+	def propagate( self, inputs, labels ):
+		"""
+		Propogates the inputs forward, calculates the gradient of loss function
+		with respect to each variable by back propogation and returns it. 
+		"""
+		nabla_W = [ np.zeros( w.shape ) for w in self.model.weights ]
+		nabla_B = [ np.zeros( b.shape ) for b in self.model.biases ]
+
+		a  = inputs 	# Activation
+		As = [inputs] 	# list to store all the Activatin matrix, layer by layer
+		Zs = []			# list to store all the z vectors, layer by layer
+
+		# Forward pass
+		for W, b in zip( self.model.weights, self.model.biases ):
+			z = np.dot( W, a ) + b
+			Zs.append(z)
+			a = self.activation(z)
+			As.append(a)
+
+		# Backwards pass / Back-Propogation
+		delta_error = self.cost.delta( As[-1], labels )
+		nabla_W[-1] = np.dot( delta_error, As[-2].transpose() )
+		nabla_B[-1] = delta_error
+
+		for l in range(2, self.num_layers):
+			delta_error = np.dot( self.model.weights[-l + 1].transpose(), delta_error ) * self.activation.prime( Zs[-l] )
+			nabla_W[-l] = np.dot( delta_error, As[-l - 1].transpose() )
+			nabla_B[-l] = delta_error
+
+		return ( nabla_W, nabla_B )
+
+# A Multi-Layers Perceptron model
+class MLP( object ):
+	def __init__( self,
+		layers        = [784,30,10],
+		optimizer     = SGD,
+		cost          = CrossEntropyCost,
+		activation    = Sigmoid,
+		learning_rate = 0.1,
+		lmbda         = 5.0,
+	):
+		self.num_layers    = len(layers)
+		self.layers        = layers
+		self.cost          = cost()
+		self.activation    = activation()
+		self.learning_rate = learning_rate
+		self.lmbda         = lmbda
+		self.optimizer     = optimizer( model=self )
+		self.weights       = [ 
 			np.random.randn(y, x) / np.sqrt(x)
 			for x, y in zip( layers[:-1], layers[1:] )
 		]
-		self.biases		= [ np.random.randn(y, 1) for y in layers[1:] ]
-
-	# Stochastic gradient decent
-	def SGD(
-		self, training_data,
+		self.biases	= [ np.random.randn(y, 1) for y in layers[1:] ]
+	
+	def fit(
+		self, 
+		training_data               = None,
+		evaluation_data             = None,
 		epochs                      = 10,
 		batch_size                  = 32,
-		learning_rate               = 0.01,
-		lmbda                       = 0.0,
-		evaluation_data             = None,
 		save                        = False,
 		continue_LastSaved          = False,
 		monitor_evaluation_cost     = False,
@@ -53,31 +137,35 @@ class MLP(object):
 		if evaluation_data :
 			n_test = len( evaluation_data )
 
-		if (continue_LastSaved):
-			model	= load_model(trained_model_file)
-			print( "Last saved, Hidden neurons: ", len(model.weights[0]), ", ", accuracy() )
-			self.weights 	= model.weights
-			self.biases 	= model.biases
-
-		outBar = tqdm( range(epochs), ncols=85, position=0 )
+		if ( continue_LastSaved ):
+			with open(trained_model_file, "r") as f:
+				data = json.load(f)
+			self.weights = [np.array(w) for w in data["weights"]]
+			self.biases  = [np.array(b) for b in data["biases"]]
+			print( "Last saved, Hidden neurons: ", len(self.weights[0]), ", ", data["accuracy"] )
+	
+		epochs_progress_bar = tqdm( range(epochs), ncols=85, position=0 )
 		stagnantCount = 0
-		for j in outBar:
-			outBar.set_description(f'Epoch {j+1}')
+		for j in epochs_progress_bar:
+			epochs_progress_bar.set_description(f'Epoch {j+1}')
 			start_T = time.time()
 			random.shuffle( training_data )
+			# Creating batches out of training data
 			batches = [
 				training_data[ k : k + batch_size ]
 				for k in range( 0, num_data, batch_size )
 			]
 
-			inBar = tqdm(batches, desc=f'Batch', ncols=75, position=1, ascii=False, leave=False)
-			for batch in inBar:
-				self.update_mini_batch( batch, learning_rate, lmbda, num_data )
+			batches_progress_bar = tqdm(batches, desc=f'Batch', ncols=75, position=1, ascii=False, leave=False)
+			# print( "old weights average: ", np.average( self.weights ) )
+			for batch in batches_progress_bar:
+				self.optimizer( batch, num_data )
+				# print( "new weights average: ", np.average( self.weights ) )
 
 			if ( save ): self.save(trained_model_file, maxAccuracy)
 
 			if monitor_training_cost:
-				cost = self.total_cost(training_data, lmbda)
+				cost = self.total_cost(training_data)
 				training_cost.append(cost)
 				tqdm.write("{}: Training cost	: {}".format(j+1,cost))
 
@@ -87,7 +175,7 @@ class MLP(object):
 				tqdm.write("   Training accuracy 	: {} / {},	{}%".format( accuracy, num_data, round((accuracy/num_data)*100, 2)))
 
 			if monitor_evaluation_cost:
-				cost = self.total_cost(evaluation_data, lmbda, convert=True)
+				cost = self.total_cost(evaluation_data, convert=True)
 				evaluation_cost.append(cost)
 				tqdm.write("   Evaluation cost 	: {}".format(cost))
 
@@ -107,66 +195,9 @@ class MLP(object):
 		print(  maxAccuracy)
 		return evaluation_cost, evaluation_accuracy, training_cost, training_accuracy
 
-	def update_mini_batch( self, batch, learning_rate, lmbda, num_data):
-		nabla_W = [ np.zeros( w.shape ) for w in self.weights ]
-		nabla_B = [ np.zeros( b.shape ) for b in self.biases ]
-
-		for inputs, labels in batch:
-			delta_Nabla_W, delta_Nabla_B = self.propagate(inputs, labels)
-
-			# Summing up all the changes in weights amd biases
-			nabla_W = [ nw + d_nw for nw, d_nw in zip( nabla_W, delta_Nabla_W ) ]
-			nabla_B = [ nb + d_nb for nb, d_nb in zip( nabla_B, delta_Nabla_B ) ]
-
-		# Taking steps towards the lowest cost function
-		# lmbda, regularization parameter
-		w_coef  = 1 - learning_rate * lmbda / num_data
-		nm_coef = learning_rate / len(batch)
-
-		self.weights = [
-			( w_coef * w ) - ( nm_coef * nw )
-			for w, nw in zip( self.weights, nabla_W )
-		]
-
-		self.biases = [ 
-			b - ( learning_rate / len(batch) ) * nb
-			for b, nb in zip( self.biases, nabla_B ) 
-		]
-
-	def propagate(self, inputs, labels):
-		"""
-		Propogates the inputs forward, calculates the gradient of loss function
-		with respect to each variable by back propogation and returns it. 
-		"""
-		nabla_W = [ np.zeros( w.shape ) for w in self.weights ]
-		nabla_B = [ np.zeros( b.shape ) for b in self.biases ]
-
-		a  = inputs 	# Activation
-		As = [inputs] 	# list to store all the Activatin matrix, layer by layer
-		Zs = []			# list to store all the z vectors, layer by layer
-
-		# Forward pass
-		for W, b in zip( self.weights, self.biases ):
-			z = np.dot( W, a ) + b
-			Zs.append(z)
-			a = self.sigmoid(z)
-			As.append(a)
-
-		# Backwards pass / Back-Propogation
-		delta_error = self.cost.delta( As[-1], labels )
-		nabla_W[-1] = np.dot( delta_error, As[-2].transpose() )
-		nabla_B[-1] = delta_error
-
-		for l in range(2, self.num_Layers):
-			delta_error = np.dot( self.weights[-l + 1].transpose(), delta_error ) * self.sigmoid_prime( Zs[-l] )
-			nabla_W[-l] = np.dot( delta_error, As[-l - 1].transpose() )
-			nabla_B[-l] = delta_error
-
-		return ( nabla_W, nabla_B )
-
 	def feed_forward( self, a ):
 		for b, w in zip(self.biases, self.weights):
-			a = self.sigmoid( np.dot( w, a ) + b )
+			a = self.activation( np.dot( w, a ) + b )
 		return a
 
 	def accuracy(self, data, convert=False):
@@ -178,42 +209,51 @@ class MLP(object):
 		                for (x, y) in tqdm(data, desc="Accuracy", ncols=75, leave=False, position=1)]
 		return sum(int(x == y) for (x, y) in results)
 
-	def total_cost(self, data, lmbda, convert=False):
+	def total_cost(self, data, convert=False):
 		cost = 0.0
 		for x, y in tqdm(data, desc="Cost", ncols=75, leave=False, position=1):
 		    a = self.feed_forward(x)
 		    if convert:
-		    	y = VectorizedResult(y)
+		    	y = vectorized_result(y)
 		    cost += self.cost.func(a, y)/len(data)
-		cost += 0.5*(lmbda/len(data))*sum(
+		cost += 0.5*( self.optimizer.lmbda / len( data ) )*sum(
 		    np.linalg.norm(w)**2 for w in self.weights)
 		return cost
 
-	def sigmoid( self, z ):
-		return 1.0 / ( 1.0 + np.exp(-z) )
-
-	def sigmoid_prime( self, z ):
-		return self.sigmoid( z ) * ( 1.0 - self.sigmoid( z ) )
-
 	def save(self, trained_model_file, accuracy):
 		data = {
-	        "layers"		: self.layers,
-	        "weights"	: [w.tolist() for w in self.weights],
-	        "biases"	: [b.tolist() for b in self.biases],
-	        "cost"		: str(self.cost.__name__),
-	        "accuracy"	: accuracy}
+	        "layers"       : self.layers,
+	        "weights"      : [ w.tolist() for w in self.weights ],
+	        "biases"       : [ b.tolist() for b in self.biases ],
+	        "optimizer"    : str(self.optimizer.__name__),
+	        "cost"         : str(self.cost.__name__),
+	        "activation"   : str(self.activation.__name__),
+	        "learning_rate": self.learning_rate,
+	        "lmbda"        : self.lmbda,
+	        "accuracy"     : accuracy,
+			}
 		with open(trained_model_file, "w") as f:
 			json.dump(data, f)
 
 def load_model( trained_model_file ):
 	with open(trained_model_file, "r") as f:
 		data = json.load(f)
-	cost = getattr(sys.modules[__name__], data["cost"])
-	net  = MLP(data["layers"], cost=cost)
 
-	net.weights = [np.array(w) for w in data["weights"]]
-	net.biases 	= [np.array(b) for b in data["biases"]]
-	return net
+	optimizer  = getattr(sys.modules[__name__], data["optimizer"])
+	cost       = getattr(sys.modules[__name__], data["cost"])
+	activation = getattr(sys.modules[__name__], data["activation"])
+	model      = MLP( 
+		data["layers"],
+		optimizer     = optimizer,
+		cost          = cost,
+		activation    = activation,
+		learning_rate = data["learning_rate"],
+		lmbda         = data["lmbda"],
+	)
+
+	model.weights = [np.array(w) for w in data["weights"]]
+	model.biases 	= [np.array(b) for b in data["biases"]]
+	return model
 
 def predict(x):
 	a = x
@@ -228,7 +268,7 @@ def model_accuracy():
 		data = json.load(f)
 	return data["accuracy"]
 
-def VectorizedResult(j):
+def vectorized_result(j):
 	e = np.zeros((10, 1))
 	e[j] = 1.0
 	return e
--- Train.py
+++ Train.py
@@ -1,31 +1,48 @@
 import mnist_loader
 import Multi_Layer_Perceptron as mlp
-from Multi_Layer_Perceptron import MLP
+from Multi_Layer_Perceptron import MLP, SGD, CrossEntropyCost, Sigmoid
 import numpy as np
-from pprint import pprint
-import time
 
 train_data, val_data, test_data = mnist_loader.load_data_wrapper()
-model = MLP( layers=[784, 30, 10] )
+model = MLP( 
+	layers        = [784, 30, 10],
+	optimizer     = SGD,
+	cost          = CrossEntropyCost,
+	activation    = Sigmoid,
+	learning_rate = 0.1,
+	lmbda         = 5.0,
+)
 	
 if 1:
 	evaluation_cost, evaluation_accuracy, \
 	training_cost, training_accuracy = \
-	model.SGD( 
+	model.fit(
 		train_data,
+		evaluation_data             = val_data,
 		epochs                      = 50,
 		batch_size                  = 32,
-		learning_rate               = 0.1,
-		lmbda                       = 5.0,
-		evaluation_data             = val_data,
 		save                        = False,
-		continue_LastSaved          = False,
+		continue_LastSaved          = True,
 		monitor_evaluation_cost     = True,
 		monitor_evaluation_accuracy = True,
 		monitor_training_cost       = True,
 		monitor_training_accuracy   = True
 	)
 
-# print( "Prediction: ", mlp.Predict( test_data[19][0] ) )
-# print( "Ground Truth: ", test_data[19][1] )
 print( mlp.model_accuracy() )
+print( "Prediction: ", mlp.predict( test_data[19][0] ) )
+print( "Ground Truth: ", test_data[19][1] )
+# print( dir(MLP) )
+
+# class A():
+# 	def __init__(self):
+# 		self.name = "Shawak"
+# class B(A):
+# 	def __init__(self, a):
+# 		super().__init__()
+# 	def print(self):
+# 		print( "Old: ", self.name )
+# 		self.name = "Saraf"
+# a = A()
+# B(a).print()
+# print( "new: ", a.name )
